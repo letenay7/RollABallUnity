0b0137a27e7450e0522fc15faf715ee619aee726
letenay7 <99814299+letenay7@users.noreply.github.com>
2024-09-27 13:40:05 +0200

    added pickups

--- RollABall/Assets/Scrpts/PayerController.cs
+++ RollABall/Assets/Scrpts/PayerController.cs
@@ -1,20 +1,24 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
+using TMPro;
 public class PayerController : MonoBehaviour
 {
     private Rigidbody rigidBody;
     private float movementX;
     private float movementY;
-    public float speed = 0; 
-
+    public float speed = 0;
+    private int count;
+    public TextMeshProUGUI countText;
+    public GameObject winTextObject;
 
     // Start is called before the first frame update
     void Start()
     {
         rigidBody = GetComponent<Rigidbody>();
-        
+        count = 0;
+        SetCountText();
+        winTextObject.SetActive(false);   
     }
-
 
     void OnMove (InputValue movementValue)
     {
@@ -24,11 +28,32 @@ public class PayerController : MonoBehaviour
 
 
     }
+
     private void FixedUpdate() 
     {
         Vector3 movement = new Vector3 (movementX, 0.0f, movementY);
         rigidBody.AddForce(movement * speed);
    
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("PickUp")) 
+        {
+            other.gameObject.SetActive(false);
+            count++;
+            SetCountText();
+            if (count >= 7) 
+            {
+                winTextObject.SetActive(true);
+
+            }
+        }
+    }
+
+     void SetCountText() 
+    {
+        countText.text =  "Score = " + count.ToString();
     }
 
 }
